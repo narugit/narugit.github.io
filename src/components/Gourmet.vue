daeba175fb15268733d39088c8ff9852fa11a508
narugit <naruiroiro@gmail.com>
2019-02-02 20:56:10 +0900

    pug2html

--- src/components/Gourmet.vue
+++ src/components/Gourmet.vue
@@ -1,8 +1,11 @@
-<template lang="pug">
-    v-container
-      v-layout(text-xs-center="" wrap="")
-        v-flex(mb-4="")
-          h1 グルメ
+<template>
+    <v-container>
+	    <v-layout text-xs-center wrap>
+            <v-flex mb-4>
+                <h1>グルメ</h1>
+            </v-flex>
+        </v-layout>
+	</v-container>
 </template>
 
 <script lang="ts">
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,32 @@
-<template lang="pug">
-    v-container
-      v-layout(text-xs-center="" wrap="")
-        v-flex(xs12="")
-          v-img.my-3(:src="require('../assets/logo.png')" contain="" height="200")
-        v-flex(mb-4="")
-          h1.display-2.font-weight-bold.mb-3 ホーム
-          p.subheading.font-weight-regular
-            | グルメが好きです
-        v-flex(xs12="" mb-5="")
-          h2.headline.font-weight-bold.mb-3 Link
-          v-layout(justify-center="")
-            a.subheading.mx-3(v-for="(eco, i) in link" :key="i" :href="eco.href" target="_blank") {{ eco.text }}
+<template>
+	<v-container>
+		<v-layout text-xs-center wrap>
+			<v-flex xs12>
+				<v-img :src="require('../assets/logo.png')" class="my-3" contain height="200"></v-img>
+			</v-flex>
+
+			<v-flex mb-4>
+				<h1 class="display-2 font-weight-bold mb-3">ホーム</h1>
+				<p class="subheading font-weight-regular">
+					グルメが好きです
+				</p>
+			</v-flex>	
+
+			<v-flex xs12 mb-5>
+				<h2 class="headline font-weight-bold mb-3">Link</h2>
+
+				<v-layout justify-center>
+					<a
+						v-for="(eco, i) in link"
+						:key="i"
+						:href="eco.href"
+						class="subheading mx-3"
+						target="_blank"
+					>{{ eco.text }}</a>
+				</v-layout>
+			</v-flex>
+		</v-layout>
+	</v-container>
 </template>
 
 <script lang="ts">
