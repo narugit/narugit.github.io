e4f96b33f4e61e9ff9880f8f78f754f4063a2283
narugit <naruiroiro@gmail.com>
2019-02-02 17:43:10 +0900

    historyモード

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,40 @@ import App from './App.vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import router from './router';
+import Router from 'vue-router';
+import Home from './components/Home.vue';
+import Gourmet from './components/Gourmet.vue';
 
 library.add(fas);
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
+
+const Routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: Home,
+  },
+  {
+    path: '/Gourmet',
+    name: 'Gourmet',
+    component: Gourmet,
+  },
+];
+
+const router = new Router({
+  routes: Routes,
+  mode: 'history',
+  linkActiveClass: 'active',
+});
+
+Vue.use(Router);
 
 new Vue({
   router,
