a177be6d3458c575fdae92f7257c18e5269d8f31
narugit <naruiroiro@gmail.com>
2019-02-02 20:12:30 +0900

    html2pug

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,26 @@
-<template>
-  <v-app>
-    <!-- ツールバー -->
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>NARKEY</span>
-        <span class="font-weight-light">のページ</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat icon @click.stop="drawer = !drawer">
-        <font-awesome-icon icon="bars" class="fa-2x"/>
-      </v-btn>
-    </v-toolbar>  
-    
-    <!-- ナビゲーションバー -->
-    <v-navigation-drawer 
-      v-model="drawer" 
-      absolute 
-      temporary 
-      right>
-
-      <v-list class="pt-0" dense>
-        <v-divider></v-divider>
-
-        <v-list-tile
-          v-for="item in items"
-          :key="item.title"
-        >
-          <v-list-tile-action>
-            <font-awesome-icon class="fa-lg" :icon="item.icon"/>
-          </v-list-tile-action>
-
-          <v-list-tile-content>
-            <v-list-tile-title>
-              <router-link class='router-txt'
-                :to="'/' + item.title">
-                {{ item.title }}
-              </router-link>
-            </v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-
-    <v-content>
-      <router-view/>
-    </v-content>
-  </v-app>
+<template lang="pug">
+    v-app
+      // ツールバー
+      v-toolbar(app="")
+        v-toolbar-title.headline.text-uppercase
+          span NARKEY
+          span.font-weight-light のページ
+        v-spacer
+        v-btn(flat="" icon="" @click.stop="drawer = !drawer")
+          font-awesome-icon.fa-2x(icon="bars")
+      // ナビゲーションバー
+      v-navigation-drawer(v-model="drawer" absolute="" temporary="" right="")
+        v-list.pt-0(dense="")
+          v-divider
+          v-list-tile(v-for="item in items" :key="item.title")
+            v-list-tile-action
+              font-awesome-icon.fa-lg(:icon="item.icon")
+            v-list-tile-content
+              v-list-tile-title
+                router-link.router-txt(:to="'/' + item.title")
+                  | {{ item.title }}
+      v-content
+        router-view
 </template>
 
 <style lang="scss">
